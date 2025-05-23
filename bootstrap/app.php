eb18c59fd0e260b654d97151f6ee32b291f40480
Paschal <paschalnwokeocha1@gmail.com>
2025-05-23 07:44:20 +0100

    crsf issue fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        // $middleware->api(prepend: [
+        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        // ]);
 
         $middleware->alias([
             'auth' => Illuminate\Auth\Middleware\Authenticate::class,
