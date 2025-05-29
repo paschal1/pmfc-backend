541c4329f3646dc4bdc15ff3ab3a542bac00d74d
Paschal <paschalnwokeocha1@gmail.com>
2025-05-29 12:49:17 +0100

    project image storage

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
